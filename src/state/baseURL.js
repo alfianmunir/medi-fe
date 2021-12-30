e854baddb70fca37b2b4a11ee24999dce360fcaf
alfianmunir <alfianmisbachulm@student.telkomuniversity.ac.id>
2021-12-30 20:22:57 +0700

    update

--- src/state/baseURL.js
+++ src/state/baseURL.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-// export default axios.create({baseURL: 'https://medicloud-id.herokuapp.com/'})
-export default axios.create({baseURL: 'http://localhost:8001/'})
+export default axios.create({baseURL: 'https://drmartinihusni.herokuapp.com/'})
+// export default axios.create({baseURL: 'http://localhost:8001/'})
