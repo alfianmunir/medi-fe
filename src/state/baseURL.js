303144a24441aaa716893d585e5712ffabefc1bd
alfianmunir <alfianmisbachulm@student.telkomuniversity.ac.id>
2022-01-01 16:38:37 +0700

    update

--- src/state/baseURL.js
+++ src/state/baseURL.js
@@ -1,3 +1,3 @@
 import axios from "axios";
-export default axios.create({baseURL: process.env.BASEURL})
+export default axios.create({baseURL: 'http://drmartinihusni.herokuapp.com/'})
 // export default axios.create({baseURL: 'http://localhost:8001/'})
